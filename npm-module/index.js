1440ba06b1b3bdb52edb73bbd5d8400e69be1b82
Kevin Nennig <knennig213@gmail.com>
2022-10-19 17:42:20 -0400

    Update index.js

--- npm-module/index.js
+++ npm-module/index.js
@@ -5,147 +5,209 @@ var minimist = require("minimist");
 var args = minimist(process.argv.slice(2));
 var path = require("path");
 
-var REPORTERS = ['emojitrain','junit','html'];
+var REPORTERS = ['emojitrain','junit', 'html'];
+
 
 var IO_COLLECTION = "https://www.getpostman.com/collections/c962d6b3b81776a4c4bf";
 var EXPORT_COLLECTION = "https://www.getpostman.com/collections/e8287cbeae23e348a791";
-// var IMPORT_COLLECTION = "https://www.getpostman.com/collections/c0c463dbe2f98d3b354a";
+var IMPORT_COLLECTION = "https://www.getpostman.com/collections/c0c463dbe2f98d3b354a";
 var DELETE_PROPS = "https://www.getpostman.com/collections/dc9e91b64f454a9b1bac";
 
-//Dev collections
-var IMPORT_COLLECTION = "https://www.getpostman.com/collections/2f3dc4c81eb464c21693";
+//Development commands
+IMPORT_COLLECTION = "https://www.getpostman.com/collections/2f3dc4c81eb464c21693";
+DELETE_PROPS = "https://www.getpostman.com/collections/357a7d9bea644bfc5b46";
+EXPORT_COLLECTION = "https://www.getpostman.com/collections/55520565b0f9933b5cf8";
+// REPORTERS = ['cli','junit', 'html'];
+
+var TIMESTAMP = formatDateTime();
 
 var init = function(yamlExports){
-  //TODO remove manual test
-  var yamlFile = yamlExports || args.f || 'tests/example-venia-tag/venia.yml';
-  //TODO delete setup
-  var deletePropStr = "";
-  if(args.delete){
-    deletePropStr = args.str || "2022";
+
+  var yamlFile = yamlExports || args.f;
+  if(!yamlFile){
+    console.log("No file specified");
+    //TODO add help
+    return;
   }
 
-  var yamlFileDir = path.dirname(path.resolve(yamlFile));
-  var file = fs.readFileSync(yamlFile, 'utf8');
-  var obj = yaml.loadAll(file)[0];
+  var fileContents = fs.readFileSync(yamlFile, 'utf8');
+  var jsonObj = "";
+  try {
+    //Attempt to read the YAML and output JSON
+    var data = yaml.loadAll(fileContents,"json");
+    var yamlContents = JSON.stringify(data[0], null, 2);
+    jsonObj = JSON.parse(yamlContents);
+  } catch {
+    console.log("Could not read YAML, attemping JSON");
+    try {
+      //Attempt to read JSON
+      jsonObj = JSON.parse(fileContents);
+    } catch(e){
+      console.log("File does not contain valid YAML or JSON content.");
+      throw e;
+    }
+  }
 
-  console.log("Name: " + obj['property-name']);
-  
-  authenicateAIO(obj, yamlFileDir, function(yamlObj){
-    //TODO export
-    
-    //import
-    createProperty(yamlObj, function(yamlObj){
-      installExtension(yamlObj, yamlFileDir, function(yamlObj){
-        importDataElements(yamlObj, yamlFileDir, function(yamlObj){
-          importRules(yamlObj, yamlFileDir, function(yamlObj){
-            publishLibrary(yamlObj, yamlFileDir, function(yamlObj){
-              var tagName = "";
-              
-              var envVals = JSON.parse(JSON.stringify(yamlObj.environment.values));
-              var environmentKey = "propFullName";
-              for(var element of envVals){
-                if(element.key.includes(environmentKey)){
-                  console.log(element.value);
-                  tagName = element.value;
-                }
-              }
-              console.log("Successfully created tag property: " + tagName);
+  console.log("Name: " + jsonObj.propertyName);
+  var yamlFileDir = path.dirname(path.resolve(yamlFile));
+  authenicateAIO(jsonObj, yamlFileDir, function(err, yamlObj){
+    if(err) throw err;
+    console.log('Connected to AIO');
+
+    if(args.export){
+      exportTag(yamlObj, yamlFileDir, function(err, yamlObj){
+        if(err) throw err;
+        console.log('Exported Tag property!');
+      });
+
+    } else if(args.import){ // import tag
+      createProperty(yamlObj, yamlFileDir, function(err, yamlObj){
+        if(err) throw err;
+        console.log('Created Tag Property!');
+
+        installExtension(yamlObj, yamlFileDir, function(err, yamlObj){
+          if(err) throw err;
+          console.log('Installed Extensions!');
+          importDataElements(yamlObj, yamlFileDir, function(err, yamlObj){
+            if(err) throw err;
+            console.log('Imported Data Elements!');
+            importRules(yamlObj, yamlFileDir, function(err, yamlObj){
+              if(err) throw err;
+          
+              publishLibrary(yamlObj, yamlFileDir, function(err, yamlObj){
+                if(err) throw err;
+                console.log("Tag Property published successfully!");
+
+                //Get the final tag property title
+                let tagName = getEnvironmentValue(yamlObj.environment, "propFullName");
+                console.log("Successfully created tag property: " + tagName);
+              });
             });
           });
         });
       });
-    });
 
-    //TODO delete
+    } else if(args.delete){
+      var deletePropStr = "";
+      if(typeof args.delete == "boolean") deletePropStr = "2022";
+      else deletePropStr = args.delete;
+      console.log("Time to delete tags that contain: " + deletePropStr);
+      deleteTags(yamlObj, yamlFileDir, deletePropStr, function(err, results){
+        if(err) throw err;
+        console.log("All tag propertys deleted with: " + deletePropStr);
+      });
+    } else {
+      console.log("No mode selected");
+      //TODO add help
+    }
 
   });
-  
   
 }
 
 // Runs the Adobe IO Token collection
-function authenicateAIO(yamlObj, yamlFileDir, callback){
+function authenicateAIO(yamlObj, workingDir, callback){
+  const name = "auth";
+  const reportName = TIMESTAMP +"-"+ name + "-Report";
+
   newman.run({
     collection: IO_COLLECTION,
-    environment: path.resolve(yamlFileDir, yamlObj.environment),
-    reporters: REPORTERS
+    environment: path.resolve(workingDir, yamlObj.environment),
+    reporters: REPORTERS,
+    reporter: {
+      'html': { export: workingDir+"/"+"newman/"+reportName+".html" },
+      'junit': { export: workingDir+"/"+"newman/"+reportName+".xml" }}
   }).on('done', function (err, summary) {
-    if (err) { throw err; }
+    if (err) { callback(err, false) }
+    
     yamlObj.environment = summary.environment;
-    console.log('Connected to IO');
-
     if(summary.run.failures == ""){
-      callback(yamlObj);
+      callback(null, yamlObj, summary);
+    } else {
+      callback(new Error("Collection Run Failures"), null, summary);
     }
   });
 }
 
 // Runs the Import Tag collection folder "Create Tag Property"
-function createProperty(yamlObj, callback){
+function createProperty(yamlObj, workingDir, callback){
+  const name = "createProp";
+  const reportName = TIMESTAMP +"-"+ name + "-Report";
   newman.run({
     collection: IMPORT_COLLECTION,
     environment: yamlObj.environment,
     folder: "Create Tag Property",
     envVar: [{
       "key": "propName",
-      "value": yamlObj['property-name']
+      "value": yamlObj.propertyName
     }],
-    reporters: REPORTERS
+    reporters: REPORTERS,
+    reporter: {
+      'html': { export: workingDir+"/"+"newman/"+reportName+".html" },
+      'junit': { export: workingDir+"/"+"newman/"+reportName+".xml" }}
   }).on('done', function (err, summary) {
     if (err) { throw err; }
     // Get the resulting environment with the token
     yamlObj.environment = summary.environment;
     if(summary.run.failures == ""){
-      console.log('Created Tag Property!'); //TODO move this and make better
-      callback(yamlObj);
+      callback(null, yamlObj, summary);
     } else {
-      console.error("Failed");
-      //TODO handle
+      callback(new Error("Collection Run Failures"), null, summary);
     }
   });
 }
 
 // Runs the Import Tag collection folder "Add Tag Extensions"
-function installExtension(yamlObj, yamlFileDir, callback){
+function installExtension(yamlObj, workingDir, callback){
+  const name = "installExts";
+  const reportName = TIMESTAMP +"-"+ name + "-Report";
+  const iterationData = getIterationData(yamlObj.extensions, workingDir, name);
+  
   newman.run({
     collection: IMPORT_COLLECTION,
     environment: yamlObj.environment,
-    globals: path.resolve(yamlFileDir, yamlObj.globals),
+    globals: path.resolve(workingDir, yamlObj.globals),
     folder: "Add Tag Extensions",
-    iterationData: path.resolve(yamlFileDir, yamlObj.extensions),
-    reporters: REPORTERS
+    iterationData: iterationData,
+    reporters: REPORTERS,
+    reporter: {
+      'html': { export: workingDir+"/"+"newman/"+reportName+".html" },
+      'junit': { export: workingDir+"/"+"newman/"+reportName+".xml" }}
   }).on('done', function (err, summary) {
     if (err) { throw err; }
     if(summary.run.failures == ""){
-      console.log('Installed Extensions!'); //TODO move this and make better
-      callback(yamlObj);
+      callback(null, yamlObj, summary);
     } else {
-      console.error("Failed");
-      //TODO Handle
+      callback(new Error("Collection Run Failures"), null, summary);
     }
     
   });
 }
 
 // Runs the Import Tag collection folder "Add Tag Data Elements"
-function importDataElements(yamlObj, yamlFileDir, callback){
+function importDataElements(yamlObj, workingDir, callback){
+  const name = "installDataElements";
+  const reportName = TIMESTAMP +"-"+ name + "-Report";
+  const iterationData = getIterationData(yamlObj.dataElements, workingDir, name);
+
   newman.run({
     collection: IMPORT_COLLECTION,
     environment: yamlObj.environment,
-    globals: path.resolve(yamlFileDir, yamlObj.globals),
+    globals: path.resolve(workingDir, yamlObj.globals),
     folder: "Add Tag Data Elements",
-    iterationData: path.resolve(yamlFileDir, yamlObj['data-elements']),
-    reporters: REPORTERS
+    iterationData: iterationData,
+    reporters: REPORTERS,
+    reporter: {
+      'html': { export: workingDir+"/"+"newman/"+reportName+".html" },
+      'junit': { export: workingDir+"/"+"newman/"+reportName+".xml" }}
   }).on('done', function (err, summary) {
     if (err) { throw err; }
     // console.log(JSON.stringify(summary.environment, null, 2));
     
     if(summary.run.failures == ""){
-      console.log('Imported Data Elements!'); //TODO move this and make better
-      callback(yamlObj);
+      callback(null, yamlObj, summary);
     } else {
-      console.error("Failed");
-      //TODO Handle
+      callback(new Error("Collection Run Failures"), null, summary);
     }
 
   });
@@ -157,13 +219,16 @@ function importDataElements(yamlObj, yamlFileDir, callback){
  *  - run newman and recurse yamlObj.rules
  *  - recursion breaks when all rules are deleted
  */
-function importRules(yamlObj, yamlFileDir, callback){
+function importRules(yamlObj, workingDir, callback){
   var ruleName = Object.keys(yamlObj.rules)[0];
   var ruleCmps = Object.values(yamlObj.rules)[0];
+  const name = ruleName;
+  const reportName = TIMESTAMP +"-"+ name + "-Report";
+  const iterationData = getIterationData(ruleCmps, workingDir, name);
 
   //Break recursion if there are no rules left
   if(ruleName === undefined){
-    callback(yamlObj);
+    callback(null, yamlObj);
   } else {
     //remove rule from yamlObj
     delete yamlObj.rules[ruleName];
@@ -172,50 +237,162 @@ function importRules(yamlObj, yamlFileDir, callback){
     newman.run({
       collection: IMPORT_COLLECTION,
       environment: yamlObj.environment,
-      globals: path.resolve(yamlFileDir, yamlObj.globals),
+      globals: path.resolve(workingDir, yamlObj.globals),
       folder: "Add Tag Rule and CMPs",
       envVar: [{
         "key": "ruleName",
         "value": ruleName
       }],
-      iterationData: path.resolve(yamlFileDir, ruleCmps),
-      reporters: REPORTERS
+      iterationData: iterationData,
+      reporters: REPORTERS,
+      reporter: {
+      'html': { export: workingDir+"/"+"newman/"+reportName+".html" },
+      'junit': { export: workingDir+"/"+"newman/"+reportName+".xml" }}
     }).on('done', function (err, summary) {
       if (err) { throw err; }
-      // console.log(JSON.stringify(summary.environment, null, 2));
       
       if(summary.run.failures == ""){
         console.log('Imported Rule: ' + ruleName);
       } else {
-        console.error("Rule Failed: " + ruleName);
+        callback(new Error("Collection Run Failures"), null);
       }
       //recurse to the next rule
-      importRules(yamlObj, yamlFileDir, callback);
+      importRules(yamlObj, workingDir, callback);
     });
   }
 }
 
 // Runs the Import Tag collection folder "Publish Tag Library"
-function publishLibrary(yamlObj, yamlFileDir, callback){
+function publishLibrary(yamlObj, workingDir, callback){
+  const name = "publishLib";
+  const reportName = TIMESTAMP +"-"+ name + "-Report";
   newman.run({
     collection: IMPORT_COLLECTION,
     environment: yamlObj.environment,
-    globals: path.resolve(yamlFileDir, yamlObj.globals),
+    globals: path.resolve(workingDir, yamlObj.globals),
     folder: "Publish Tag Library",
-    reporters: REPORTERS
+    reporters: REPORTERS,
+    reporter: {
+      'html': { export: workingDir+"/"+"newman/"+reportName+".html" },
+      'junit': { export: workingDir+"/"+"newman/"+reportName+".xml" }}
   }).on('done', function (err, summary) {
     if (err) { throw err; }
 
-
-  
     if(summary.run.failures == ""){
-      console.log("Tag Property published successfully!");
-      callback(yamlObj);
+      callback(null, yamlObj, summary);
     } else {
-      console.error("Publishing Failed");
-      //TODO handle
+      callback(new Error("Collection Run Failures"), null, summary);
     }
   });
+}
+
+function deleteTags(yamlObj, workingDir, str, callback){
+  const name = "deleteTags";
+  const reportName = TIMESTAMP +"-"+ name + "-Report";
+  newman.run({
+    collection: DELETE_PROPS,
+    environment: yamlObj.environment,
+    envVar: [{
+      "key": "tagNameIncludes",
+      "value": str
+    }],
+    reporters: REPORTERS,
+    reporter: {
+      'html': { export: workingDir+"/"+"newman/"+reportName+".html" },
+      'junit': { export: workingDir+"/"+"newman/"+reportName+".xml" }}
+  }).on('done', function (err, summary) {
+    if (err) { throw err; }
+    if(summary.run.failures == ""){
+      callback(null, yamlObj, summary);
+    } else {
+      callback(new Error("Collection Run Failures"), null, summary);
+    }
+  });
+}
+
+function exportTag(yamlObj, workingDir, callback){
+  //newman run $EXPORT_COLLECTION -e $ENVIRONMENT --env-var "propID=$propID"
+  const name = "exportTag";
+  const reportName = TIMESTAMP +"-"+ name + "-Report";
+  newman.run({
+    collection: EXPORT_COLLECTION,
+    environment: yamlObj.environment,
+    envVar: [{
+      "key": "propID",
+      "value": yamlObj.exportPropID
+    }],
+    reporters: REPORTERS,
+    reporter: {
+      'html': { export: workingDir+"/"+"newman/"+reportName+".html" },
+      'junit': { export: workingDir+"/"+"newman/"+reportName+".xml" }}
+  }).on('done', function (err, summary) {
+    if (err) { throw err; }
+    if(summary.run.failures == ""){
+      let tagExport = {};
+      tagExport.extensions = getEnvironmentValue(summary.environment, "exportExtensions");
+      tagExport.dataElements = getEnvironmentValue(summary.environment, "exportDataElements");
+      tagExport.ruleNames = getEnvironmentValue(summary.environment, "exportRules");
+      // fs.writeFileSync('./env.json', JSON.stringify(summary.environment, null, 2) , 'utf-8');
+      tagExport.rules = {};
+      for(var element in tagExport.ruleNames){
+        let ruleName = tagExport.ruleNames[element].attributes.name
+        // console.log(tagExport.ruleNames[element].attributes.name);
+        tagExport.rules[ruleName] = getEnvironmentValue(summary.environment, "exportRuleCmps-"+element);
+      }
+
+      //TODO pull out propertyName from Export to write
+      propertyName = "export";
+      //Write to a file
+      fs.writeFileSync(workingDir+"/"+propertyName+".json", JSON.stringify(tagExport,null,2));
+      fs.writeFileSync(workingDir+"/"+propertyName+".yml", yaml.dump(tagExport));
+
+
+      callback(null, yamlObj, summary);
+    } else {
+      callback(new Error("Collection Run Failures"), null, summary);
+    }
+  });
+}
+
+/******* Helper Functions ******/
+
+function formatDateTime() {
+  var d = new Date(Date.now());
+  var year = d.getFullYear();
+  var month = d.getMonth() + 1;
+  var date = d.getDate();
+  var hour = d.getHours();
+  var min = (d.getMinutes() < 10 ? '0' : '') + d.getMinutes();
+  var sec = (d.getSeconds() < 10 ? '0' : '') + d.getSeconds();
+  var time = year + "-" + month + "-" + date + "-" + hour + ':' + min;
+  return time;
+}
+
+function getEnvironmentValue(envObj, key){
+  let envVals = JSON.parse(JSON.stringify(envObj.values));
+  for(var element of envVals){
+    if(element.key == key){
+      // console.log(element.value);
+      return element.value;
+    }
+  }
+  return "";
+}
+
+function getIterationData(iterationData, rootDir, dataName){
+  try {
+    if(typeof iterationData == "string"){
+      let absPath = path.resolve(rootDir, iterationData);
+      if(fs.existsSync(iterationData)){
+        return absPath;
+      }
+    } else {
+      return iterationData;
+    }
+  } catch (err){
+    // callback(err, "Iteration Data for "+dataName+ " does not exist.");
+    return err;
+  }
 }
 
 exports.run = init;
